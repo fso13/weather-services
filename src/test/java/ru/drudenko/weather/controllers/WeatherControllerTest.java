b391a8f7c6d81e478f6ce169ab2ef76d6c335513
Dmitry Rudenko <drudenk@roox.ru>
2021-08-06 12:27:22 +0300

    review docs

--- src/test/java/ru/drudenko/weather/controllers/WeatherControllerTest.java
+++ src/test/java/ru/drudenko/weather/controllers/WeatherControllerTest.java
@@ -8,11 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.restdocs.ManualRestDocumentation;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.operation.preprocess.Preprocessors;
 import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.request.RequestDocumentation;
+import org.springframework.restdocs.snippet.Attributes;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -21,28 +22,19 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import ru.drudenko.weather.TestConfig;
-import ru.drudenko.weather.internal.openweathermap.dto.City;
-import ru.drudenko.weather.internal.openweathermap.dto.List;
-import ru.drudenko.weather.internal.openweathermap.dto.Main;
-import ru.drudenko.weather.internal.openweathermap.dto.Result;
-import ru.drudenko.weather.internal.openweathermap.dto.Wind;
 
-import java.util.ArrayList;
 import java.util.Base64;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(WeatherController.class)
 @Import({TestConfig.class})
-@AutoConfigureRestDocs(uriScheme = "https", uriHost = "ru.drudenko.weather", uriPort = 80)
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "https://weather-services.herokuapp.com/", uriPort = 443)
 public class WeatherControllerTest {
     public static final String CITY = "Москва";
-    public static final String ENDPOINT_WEATHER_CITY = "/weather?city={name}";
-    public static final String ENDPOINT_WEATHER_LON_LAT = "/weather?lon={lon}&lat={lat}";
+    public static final String ENDPOINT_WEATHER = "/weather";
     public static final String VALID_CREDENTIALS_HEADER = "Basic " + Base64.getEncoder().encodeToString("user1:1111".getBytes());
     public static final String NO_VALID_CREDENTIALS_HEADER = "Basic " + Base64.getEncoder().encodeToString("user1:fail".getBytes());
-    private final ManualRestDocumentation restDocumentation = new ManualRestDocumentation();
-    @Autowired
-    private WebApplicationContext context;
+    ;
     @Autowired
     private MockMvc mockMvc;
 
@@ -59,13 +51,17 @@ public class WeatherControllerTest {
     @Test
     public void getWeatherByCityOk() throws Exception {
 
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_WEATHER_CITY, CITY)
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_WEATHER)
+                        .param("city", CITY)
                         .accept("application/json")
                         .contentType("application/json")
+                        .characterEncoding("UTF-8")
                         .header("authorization", VALID_CREDENTIALS_HEADER))
                 .andDo(MockMvcRestDocumentation.document("{method-name}",
                         Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
                         Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+                        RequestDocumentation.requestParameters(RequestDocumentation.parameterWithName("city").description(
+                                        "Название города").attributes(Attributes.key("constraints").value("Обязательное"))),
                         PayloadDocumentation.responseFields(PayloadDocumentation.fieldWithPath("city").description(
                                 "Название города"), PayloadDocumentation.fieldWithPath("temperature").description(
                                 "Температура"), PayloadDocumentation.fieldWithPath("wind").description(
@@ -82,16 +78,20 @@ public class WeatherControllerTest {
     @Test
     public void getWeatherByCityBadRequest() throws Exception {
 
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_WEATHER_CITY, "")
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_WEATHER)
+                        .param("city", "")
                         .accept("application/json")
                         .contentType("application/json")
+                        .characterEncoding("UTF-8")
                         .header("authorization", VALID_CREDENTIALS_HEADER))
                 .andDo(MockMvcRestDocumentation.document("{method-name}",
                         Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
                         Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+                        RequestDocumentation.requestParameters(RequestDocumentation.parameterWithName("city").description(
+                                "Название города").attributes(Attributes.key("constraints").value("Обязательное"))),
                         PayloadDocumentation.responseFields(PayloadDocumentation.fieldWithPath("code").description(
-                                "Http code"), PayloadDocumentation.fieldWithPath("message").description(
-                                "Error message"))));
+                                "Http код"), PayloadDocumentation.fieldWithPath("message").description(
+                                "Описание ошибки"))));
 
         resultActions.andExpect(MockMvcResultMatchers.status().isBadRequest())
                 .andExpect(MockMvcResultMatchers.jsonPath("code", CoreMatchers.is(400)))
@@ -101,16 +101,20 @@ public class WeatherControllerTest {
     @Test
     public void getWeatherByCityBadCredentials() throws Exception {
 
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_WEATHER_CITY, CITY)
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_WEATHER)
+                        .param("city", CITY)
                         .accept("application/json")
                         .contentType("application/json")
+                        .characterEncoding("UTF-8")
                         .header("authorization", NO_VALID_CREDENTIALS_HEADER))
                 .andDo(MockMvcRestDocumentation.document("{method-name}",
                         Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
                         Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+                        RequestDocumentation.requestParameters(RequestDocumentation.parameterWithName("city").description(
+                                "Название города").attributes(Attributes.key("constraints").value("Обязательное"))),
                         PayloadDocumentation.responseFields(PayloadDocumentation.fieldWithPath("code").description(
-                                "Http code"), PayloadDocumentation.fieldWithPath("message").description(
-                                "Error message"))));
+                                "Http код"), PayloadDocumentation.fieldWithPath("message").description(
+                                "Описание ошибки"))));
 
         resultActions.andExpect(MockMvcResultMatchers.status().isUnauthorized())
                 .andExpect(MockMvcResultMatchers.jsonPath("code", CoreMatchers.is(401)))
@@ -120,13 +124,20 @@ public class WeatherControllerTest {
     @Test
     public void getWeatherByCoordinatesOk() throws Exception {
 
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_WEATHER_LON_LAT, 20.0,  30.0)
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_WEATHER)
+                        .param("lon", "20.0")
+                        .param("lat", "30.0")
                         .accept("application/json")
                         .contentType("application/json")
+                        .characterEncoding("UTF-8")
                         .header("authorization", VALID_CREDENTIALS_HEADER))
                 .andDo(MockMvcRestDocumentation.document("{method-name}",
                         Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
                         Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+                        RequestDocumentation.requestParameters(RequestDocumentation.parameterWithName("lon").description(
+                                "Долгота").attributes(Attributes.key("constraints").value("Обязательное")),
+                                RequestDocumentation.parameterWithName("lat").description(
+                                        "Широта").attributes(Attributes.key("constraints").value("Обязательное"))),
                         PayloadDocumentation.responseFields(PayloadDocumentation.fieldWithPath("city").description(
                                 "Название города"), PayloadDocumentation.fieldWithPath("temperature").description(
                                 "Температура"), PayloadDocumentation.fieldWithPath("wind").description(
@@ -142,44 +153,30 @@ public class WeatherControllerTest {
     @Test
     public void getWeatherByCoordinatesBadCredentials() throws Exception {
 
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_WEATHER_LON_LAT, 20.0, 30.0)
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT_WEATHER)
+                        .param("lon", "20.0")
+                        .param("lat", "30.0")
                         .accept("application/json")
                         .contentType("application/json")
+                        .characterEncoding("UTF-8")
                         .header("authorization", NO_VALID_CREDENTIALS_HEADER))
                 .andDo(MockMvcRestDocumentation.document("{method-name}",
                         Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
                         Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+                        RequestDocumentation.requestParameters(RequestDocumentation.parameterWithName("lon").description(
+                                        "Долгота").attributes(Attributes.key("constraints").value("Обязательное")),
+                                RequestDocumentation.parameterWithName("lat").description(
+                                        "Широта").attributes(Attributes.key("constraints").value("Обязательное"))),
                         PayloadDocumentation.responseFields(PayloadDocumentation.fieldWithPath("code").description(
-                                "Http code"), PayloadDocumentation.fieldWithPath("message").description(
-                                "Error message"))));
+                                "Http код"), PayloadDocumentation.fieldWithPath("message").description(
+                                "Описание ошибки"))));
 
         resultActions.andExpect(MockMvcResultMatchers.status().isUnauthorized())
                 .andExpect(MockMvcResultMatchers.jsonPath("code", CoreMatchers.is(401)))
                 .andExpect(MockMvcResultMatchers.jsonPath("message", CoreMatchers.is("Bad credentials")));
     }
+
     @Test
     public void contextLoads() {
-    }
-
-    private Result getStubResult() {
-        Result result = new Result();
-        City city = new City();
-        city.setName(CITY);
-        result.setCity(city);
-
-        java.util.List<List> listResult = new ArrayList<>();
-        List e = new List();
-        Main main = new Main();
-        main.setTemp(293.0);
-        Wind wind = new Wind();
-        wind.setSpeed(3.0);
-        wind.setDeg(133.0);
-
-        e.setMain(main);
-        e.setWind(wind);
-        listResult.add(e);
-
-        result.setList(listResult);
-        return result;
     }
 }
